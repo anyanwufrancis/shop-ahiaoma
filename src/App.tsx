1e32051f99fa423f7ce2f0284c0a0375ace5b615
anyanwufrancis <anyanwufrancis40@gmail.com>
2026-01-06 15:56:05 +0100

    Fix router setup and Vercel blank page

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,7 @@
-import {
-  //  ChakraProvider,
-    Box, Flex, Image } from "@chakra-ui/react";
-import {
- Router,
-  Routes,
-  Route,
-  useLocation,
-} from "react-router-dom";
-// import Header from "./Components/Header";
+import { Flex, Image } from "@chakra-ui/react";
+import { Routes, Route } from "react-router-dom";
+import { useState, useEffect } from "react";
+
 import Home from "./Components/Home";
 import Gallery from "./Components/Gallery";
 import About from "./Components/about/About";
@@ -15,19 +9,9 @@ import Contact from "./Components/Contact";
 import Blog from "./Components/Blog";
 import Login from "./Components/Login";
 import ForgotPassword from "./Components/ForgotPassword";
-import { useState, useEffect } from "react";
 import { Work } from "./Components/work";
 
 function AppContent() {
-  const location = useLocation();
-
-  // Normalize path (remove trailing slash, lowercase)
-  const currentPath = location.pathname.toLowerCase().replace(/\/$/, "");
-
-  // Pages without header/footer
-  const hideHeaderFooter =
-    currentPath === "/login" || currentPath === "/forgotpassword";
-
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
@@ -38,41 +22,27 @@ function AppContent() {
   if (loading) {
     return (
       <Flex align="center" justify="center" height="100vh" bg="white">
-        <Image src="src/assets/45.svg" alt="Loading..." boxSize="50px" />
+        <Image src="/45.svg" alt="Loading..." boxSize="50px" />
       </Flex>
     );
   }
 
   return (
     <>
-      {/* {!hideHeaderFooter && <Header />} */}
-
-      <Box as="main" mt={!hideHeaderFooter ? 4 : 0}>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/gallery" element={<Gallery />} />
-          <Route path="/work" element={<Work />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/blog" element={<Blog />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/forgotpassword" element={<ForgotPassword />} />
-        </Routes>
-      </Box>
-
-      {/* {!hideHeaderFooter && <Footer />} */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/gallery" element={<Gallery />} />
+        <Route path="/work" element={<Work />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/forgotpassword" element={<ForgotPassword />} />
+      </Routes>
     </>
   );
 }
 
-function App() {
-  return (
-    // <ChakraProvider>
-      // <Router>
-        <AppContent />
-      // </Router>
-    // {/* </ChakraProvider> */}
-  );
+export default function App() {
+  return <AppContent />;
 }
-
-export default App;
